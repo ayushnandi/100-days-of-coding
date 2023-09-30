2362e232e68c655c6ad67c920c392f351b0ce630
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2023-09-30 17:44:09 +0530

    Plus One ,  Path Sum , Valid Anagram , Remove Linked List Elements ,  Container With Most Water , Palindrome Number ,  Valid Parentheses , Can Place Flowers , Minimum Depth of Binary Tree

---
+++ 11. Path Sum.java
@@ -0,0 +1,65 @@
+/*
+11. Path Sum
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+Example 1:
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Explanation: The root-to-leaf path with the target sum is shown.
+
+Example 2:
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Explanation: There two root-to-leaf paths in the tree:
+(1 --> 2): The sum is 3.
+(1 --> 3): The sum is 4.
+There is no root-to-leaf path with sum = 5.
+
+Example 3:
+
+Input: root = [], targetSum = 0
+Output: false
+Explanation: Since the tree is empty, there are no root-to-leaf paths.
+*/
+
+// Solution
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return SumCheck(root, targetSum, 0);
+    }
+    public boolean SumCheck(TreeNode root, int targetSum, int sum) {
+        if (root == null) {
+            return false;
+        }
+        sum = sum + root.val;
+        if (sum == targetSum && root.left==null && root.right==null){
+            return true;
+        }
+            return SumCheck(root.left,targetSum,sum)||SumCheck(root.right,targetSum,sum);
+        
+    }
+}
+
+
